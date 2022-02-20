eeab8b5d6fa8d439fe7bc3536b3e7a13edfc04ac
brianforhan <brian.forhan@kellogg.northwestern.edu>
2022-02-20 18:58:55 +0000

    commit #2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,15 @@
 class PostsController < ApplicationController
+    
+    def new
+        @post = Post.new
+        @places = Places.find(params["post_id"])
+        @post.places_id = @post.id
+    end
+    
+    def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
+    end
+    
 end
